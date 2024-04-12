3f70e91dedca268e333d233457c20695f0b4f6a7
iam-meddi <116271441+iam-meddi@users.noreply.github.com>
2024-04-12 15:57:05 +0300

    Update in the profile page

--- greenhouse_project/lib/pages/profile.dart
+++ greenhouse_project/lib/pages/profile.dart
@@ -6,20 +6,44 @@ library;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:greenhouse_project/services/cubit/home_cubit.dart';
 import 'package:greenhouse_project/utils/buttons.dart';
 import 'package:greenhouse_project/utils/text_styles.dart';
 import 'package:greenhouse_project/utils/theme.dart';
 
-class ProfilePage extends StatefulWidget {
+class ProfilePage extends StatelessWidget {
   final UserCredential userCredential;
 
   const ProfilePage({super.key, required this.userCredential});
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+
+        BlocProvider(
+          create: (context) => NotificationsCubit(userCredential),
+        ),
+        BlocProvider(
+          create: (context) => UserInfoCubit(),
+        ),
+
+      ],
+      child: _ProfilePageContent(userCredential: userCredential),
+    );
+  }
+}
+class _ProfilePageContent extends StatefulWidget {
+  final UserCredential userCredential;
+
+  const _ProfilePageContent ({super.key, required this.userCredential});
+
+  @override
+  State<_ProfilePageContent> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends State<_ProfilePageContent> {
   // Define custom theme
   final ThemeData customTheme = theme;
 
@@ -37,97 +61,101 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   // Define variables for user information
-  late String _userName = "";
 
   @override
   void initState() {
+    context.read<UserInfoCubit>().getUserInfo(widget.userCredential);
     super.initState();
-    _getUserInfo();
   }
 
-  // Retrieve user information from Firebase
-  Future<void> _getUserInfo() async {
-    String? email = widget.userCredential.user?.email;
-    QuerySnapshot userQuery = await FirebaseFirestore.instance
-        .collection('users')
-        .where('email', isEqualTo: email)
-        .get();
-
-    if (userQuery.docs.isNotEmpty) {
-      DocumentSnapshot userSnapshot = userQuery.docs.first;
-      Map<String, dynamic>? userData =
-          userSnapshot.data() as Map<String, dynamic>?;
-
-      setState(() {
-        _userName = userData?['name'] ?? 'Unknown';
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: customTheme,
-        home: Scaffold(
-          appBar: AppBar(
-              leading: IconButton(
-                  onPressed: () => Navigator.pop(context),
-                  icon: Image.asset(
-                    "lib/utils/Icons/Left Arrow.png",
-                    scale: 3,
-                  )),
-              title: const Padding(
-                padding: EdgeInsets.fromLTRB(70, 0, 0, 0),
-                child: Text(
-                  "Profile",
-                  style: headingTextStyle,
+    return BlocConsumer<UserInfoCubit, HomeState>(
+      listener: (context, state){},
+      builder: (context, state){
+        if (state is UserInfoLoading){
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        else if (state is UserInfoLoaded){
+          return MaterialApp(
+              theme: customTheme,
+              home: Scaffold(
+                appBar: AppBar(
+                    leading: IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: Image.asset(
+                          "lib/utils/Icons/Left Arrow.png",
+                          scale: 3,
+                        )),
+                    title: const Padding(
+                      padding: EdgeInsets.fromLTRB(70, 0, 0, 0),
+                      child: Text(
+                        "Profile",
+                        style: headingTextStyle,
+                      ),
+                    )),
+                body: Column(
+                  children: [
+                    const Center(child: Text("PROFILE PIC")),
+                    const Text("Name"),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.green[400],
+                            labelText: state.userName,
+                            border: const OutlineInputBorder(),
+                          )),
+                    ),
+                    const Text("Email"),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.green[400],
+                            labelText: widget.userCredential.user?.email,
+                            border: const OutlineInputBorder(),
+                          )),
+                    ),
+                    const Text("Password"),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.green[400],
+                            labelText: '********',
+                            border: const OutlineInputBorder(),
+                          )),
+                    ),
+                    Align(
+                        alignment: Alignment.bottomCenter,
+                        child: GreenElevatedButton(text: "Edit", onPressed: () {}))
+                  ],
                 ),
-              )),
-          body: Column(
-            children: [
-              const Center(child: Text("PROFILE PIC")),
-              const Text("Name"),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.green[400],
-                      labelText: _userName,
-                      border: const OutlineInputBorder(),
-                    )),
-              ),
-              const Text("Email"),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.green[400],
-                      labelText: widget.userCredential.user?.email,
-                      border: const OutlineInputBorder(),
-                    )),
-              ),
-              const Text("Password"),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.green[400],
-                      labelText: '********',
-                      border: const OutlineInputBorder(),
-                    )),
-              ),
-              Align(
-                  alignment: Alignment.bottomCenter,
-                  child: GreenElevatedButton(text: "Edit", onPressed: () {}))
-            ],
-          ),
-        ));
+              ));
+        }
+        else if (state is UserInfoError) {
+          return Center(child: Text('Error: ${state.errorMessage}'));
+        }
+        // Should never happen; but, you never know
+        else {
+          return const Center(
+            child: Text('Unexpected state'),
+          );
+        }
+      },
+
+    );
+
   }
 }
