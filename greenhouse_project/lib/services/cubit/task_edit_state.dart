79555de5b9cab7a9bed5a002f512490f9e6f7871
Ali Farrokhnejad <farrokh445@gmail.com>
2024-05-09 14:07:21 +0300

    Management page progress Added task creation button (finished)

--- greenhouse_project/lib/services/cubit/task_edit_state.dart
+++
@@ -1,8 +0,0 @@
-// part of 'task_edit_cubit.dart';
-
-// @immutable
-// sealed class TaskEditState {}
-
-// final class TaskEditInitial extends TaskEditState {}
-
-// final class TaskEditValid extends TaskEditState {}
