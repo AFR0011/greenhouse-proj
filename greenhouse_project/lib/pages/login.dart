dadffa3a524f3f3ccf50f6f99f63122fe3d3a304
iam-meddi <116271441+iam-meddi@users.noreply.github.com>
2024-05-12 17:09:36 +0300

    login page design

--- greenhouse_project/lib/pages/login.dart
+++ greenhouse_project/lib/pages/login.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:greenhouse_project/services/cubit/auth_cubit.dart';
+import 'package:greenhouse_project/utils/bakground.dart';
 import 'package:greenhouse_project/utils/text_styles.dart';
 import 'home.dart';
 
@@ -47,49 +48,6 @@ class _LoginPageState extends State<LoginPage>
   @override
   void initState() {
     super.initState();
-
-    // Background animation
-    _animController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 5));
-    _topAlignmentAnimation = TweenSequence<Alignment>([
-      TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.topLeft, end: Alignment.topRight),
-          weight: 1),
-      TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.topRight, end: Alignment.bottomRight),
-          weight: 1),
-      TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.bottomRight, end: Alignment.bottomLeft),
-          weight: 1),
-      TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.bottomLeft, end: Alignment.topLeft),
-          weight: 1),
-    ]).animate(_animController);
-    _bottomAlignmentAnimation = TweenSequence<Alignment>([
-      TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.bottomRight, end: Alignment.bottomLeft),
-          weight: 1),
-      TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.bottomLeft, end: Alignment.topLeft),
-          weight: 1),
-      TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.topLeft, end: Alignment.topRight),
-          weight: 1),
-      TweenSequenceItem<Alignment>(
-          tween: Tween<Alignment>(
-              begin: Alignment.topRight, end: Alignment.bottomRight),
-          weight: 1),
-    ]).animate(_animController);
-
-    // Repeat animation
-    _animController.repeat();
   }
 
   @override
@@ -105,21 +63,7 @@ class _LoginPageState extends State<LoginPage>
           }
           return Stack(children: [
             // Background
-            AnimatedBuilder(
-              animation: _animController,
-              builder: (context, _) {
-                return Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: const [
-                        Color.fromARGB(255, 0, 255, 162),
-                        Color.fromARGB(255, 0, 130, 16),
-                      ],
-                          begin: _topAlignmentAnimation.value,
-                          end: _bottomAlignmentAnimation.value)),
-                );
-              },
-            ),
+            SeaBackground(),
             // Content
             Padding(
               padding: const EdgeInsets.all(25.0),
