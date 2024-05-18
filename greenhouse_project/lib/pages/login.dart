8b747e34a08dd7ff01e4a728fbbd41f8f141ca6b
iam-meddi <116271441+iam-meddi@users.noreply.github.com>
2024-05-18 19:04:53 +0300

    Update login.dart

--- greenhouse_project/lib/pages/login.dart
+++ greenhouse_project/lib/pages/login.dart
@@ -11,7 +11,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:greenhouse_project/services/cubit/auth_cubit.dart';
 import 'package:greenhouse_project/utils/bakground.dart';
+import 'package:greenhouse_project/utils/input.dart';
 import 'package:greenhouse_project/utils/text_styles.dart';
+import 'package:greenhouse_project/utils/theme.dart';
 import 'home.dart';
 
 class LoginPage extends StatefulWidget {
@@ -47,93 +49,117 @@ class _LoginPageState extends State<LoginPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<AuthCubit, AuthState>(
-        builder: (context, state) {
-          // Show "loading screen" if auth request is being processed
-          if (state is AuthLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return Stack(children: [
-            // Background
-            SeaBackground(),
-            // Content
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child:SafeArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("lib/utils/Icons/Logo.png", width: 90, height: 90),
-                  
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 60),
-                      child: Text(
-                        "Greenhouse Control System",
-                        style: headingTextStyle,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white70,
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    TextField(
-                      controller: passwordController,
-                      obscureText:  _isSecurePassword,
-                      decoration:  InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white70,
-                        labelText: 'Password',
-                        border: OutlineInputBorder(),
-                        suffixIcon: togglePassword(),
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        context.read<AuthCubit>().authLoginRequest(
-                            emailController.text.trim(), passwordController.text);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 50, vertical: 15),
-                      ),
-                      child: const Text('Login'),
-                    ),
-                  ],
-                ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: CustomPaint(
+              painter: WavePainter1(),
+              child: Container(
+                height: 300,
               ),
             ),
-          ]);
-        },
-        listener: (context, state) {
-          // If authentication is successful, navigate to home page
-          if (state is AuthSuccess) {
-            Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        HomePage(userCredential: state.userCredential)),
-                (route) => false);
-            // If authentication is unsuccessful, display error message
-          } else if (state is AuthFailure) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.error!),
-                backgroundColor: Colors.red,
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: CustomPaint(
+              painter: WavePainter(),
+              child: Container(
+                height: 300,
               ),
-            );
-          }
-        },
-      ),
+            ),
+          ),
+          BlocConsumer<AuthCubit, AuthState>(
+          builder: (context, state) {
+            // Show "loading screen" if auth request is being processed
+            if (state is AuthLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return Stack(children: [
+              // Background
+              // Content
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child:SafeArea(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      
+                    
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 60),
+                        child: Text(
+                          "Greenhouse Control System",
+                          style: headingTextStyle,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: theme.colorScheme.secondary,
+                          labelText: 'Email',
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      TextField(
+                        controller: passwordController,
+                        obscureText:  _isSecurePassword,
+                        decoration:  InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white70,
+                          labelText: 'Password',
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                          suffixIcon: togglePassword(),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          context.read<AuthCubit>().authLoginRequest(
+                              emailController.text.trim(), passwordController.text);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 15),
+                        ),
+                        child: const Text('Login'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]);
+          },
+          listener: (context, state) {
+            // If authentication is successful, navigate to home page
+            if (state is AuthSuccess) {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          HomePage(userCredential: state.userCredential)),
+                  (route) => false);
+              // If authentication is unsuccessful, display error message
+            } else if (state is AuthFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.error!),
+                  backgroundColor: Colors.red,
+                ),
+              );
+            }
+          },
+        ),
+     ] ),
     );
   }
   Widget togglePassword(){
@@ -147,3 +173,4 @@ class _LoginPageState extends State<LoginPage>
 
     }
 }
+
