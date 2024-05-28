3b598702de888121ca2776e52ec7d6cb46a9f6ec
Ali Farrokhnejad <farrokh445@gmail.com>
2024-05-28 12:27:39 +0300

    Backend API setup for Heroku

--- greenhouse_project/lib/utils/chart.dart
+++ greenhouse_project/lib/utils/chart.dart
@@ -15,75 +15,72 @@ import "package:flutter/material.dart";
 class ChartClass extends StatelessWidget {
   final double miny, maxy;
   final List values;
-  const ChartClass({super.key, required this.maxy, required this.miny, required this.values});
+  const ChartClass(
+      {super.key,
+      required this.maxy,
+      required this.miny,
+      required this.values});
 
   @override
   Widget build(BuildContext context) {
     print("Values $values");
     return SizedBox(
-      height: MediaQuery.of(context).size.height*.5,
       width: MediaQuery.of(context).size.width * 0.9,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: LineChart(
-          LineChartData(
+        child: LineChart(LineChartData(
             minX: 0,
             maxX: 24,
             minY: miny,
             maxY: maxy,
             titlesData: const FlTitlesData(
-              show: true,
-              bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
+                show: true,
+                bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
                   showTitles: true,
                   interval: 4,
                   reservedSize: 30,
-                )
-              ),
-              topTitles: AxisTitles(sideTitles:SideTitles(
-                showTitles: false,
-              )),
-              rightTitles: AxisTitles(sideTitles: SideTitles(
-                showTitles: false,
-                
-              ),
-            )
-              
-            ),
-            borderData: FlBorderData(
-              show: true,
-              border: Border.all()
-            ),
-            gridData:FlGridData(
-              show: true,
-              getDrawingHorizontalLine: (value){
-                return const FlLine(
-                  color: Colors.amber,
-                  strokeWidth:.5,
-
-                );
-              }
-            ) ,
-            lineBarsData: [LineChartBarData(
-              dotData: const FlDotData(show: false),
-              spots: values.map((value) => createSpot(values.indexOf(value), value)).toList(),
-              isCurved: true,
-              gradient: const LinearGradient(colors: [Colors.red,Colors.blue]),
-              belowBarData: BarAreaData(
-                
+                )),
+                topTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                  showTitles: false,
+                )),
+                rightTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: false,
+                  ),
+                )),
+            borderData: FlBorderData(show: true, border: Border.all()),
+            gridData: FlGridData(
                 show: true,
-                gradient: LinearGradient(colors: [Colors.red.withOpacity(.1),Colors.blue.withOpacity(0.1)]),
-              )
-
-            ),]
-          )
-        ),
+                getDrawingHorizontalLine: (value) {
+                  return const FlLine(
+                    color: Colors.amber,
+                    strokeWidth: .5,
+                  );
+                }),
+            lineBarsData: [
+              LineChartBarData(
+                  dotData: const FlDotData(show: false),
+                  spots: values
+                      .map((value) => createSpot(values.indexOf(value), value))
+                      .toList(),
+                  isCurved: true,
+                  gradient:
+                      const LinearGradient(colors: [Colors.red, Colors.blue]),
+                  belowBarData: BarAreaData(
+                    show: true,
+                    gradient: LinearGradient(colors: [
+                      Colors.red.withOpacity(.1),
+                      Colors.blue.withOpacity(0.1)
+                    ]),
+                  )),
+            ])),
       ),
     );
-
   }
 }
 
 FlSpot createSpot(index, value) {
-   return FlSpot(index*4.roundToDouble(), value.roundToDouble());
+  return FlSpot(index * 4.roundToDouble(), value.roundToDouble());
 }
